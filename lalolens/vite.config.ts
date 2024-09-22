dd3c177c6eb4342483b3659550dcbf4a423b9911
lalolens <elaloglens@gmail.com>
2024-09-22 22:51:00 +0000

    adding github pages

--- lalolens/vite.config.ts
+++ lalolens/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../www', // Replace 'build' with your desired output directory name
+    outDir: '..', // Replace 'build' with your desired output directory name
   },
 })
