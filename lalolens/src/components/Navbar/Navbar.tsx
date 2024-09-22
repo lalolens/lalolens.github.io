c00ed26ec2d0a8b17a4d7a8f186fbc6707ba37e8
lalolens <elaloglens@gmail.com>
2024-09-22 23:50:04 +0000

    Deployment pipelne setup

--- lalolens/src/components/Navbar/Navbar.tsx
+++ lalolens/src/components/Navbar/Navbar.tsx
@@ -1,14 +1,35 @@
 // src/components/Navbar.tsx
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 // Import the logo image
 import logo from '../../assets/lalolenslogo.svg'; // Adjust the path if necessary
 
 const Navbar: React.FC = () => {
+  const location = useLocation();
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 50) { // Adjust the scroll offset as needed
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    
+    // Cleanup listener on unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
       <div className="navbar__logo">
         <Link to="/">
           <img src={logo} alt="Logo" />
@@ -16,16 +37,40 @@ const Navbar: React.FC = () => {
       </div>
       <ul className="navbar__links">
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            to="/"
+            className={location.pathname === '/' ? 'active' : ''}
+            aria-label="Home"
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link
+            to="/about"
+            className={location.pathname === '/about' ? 'active' : ''}
+            aria-label="About Us"
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/projects">Projects</Link>
+          <Link
+            to="/projects"
+            className={location.pathname === '/projects' ? 'active' : ''}
+            aria-label="Our Projects"
+          >
+            Projects
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link
+            to="/contact"
+            className={location.pathname === '/contact' ? 'active' : ''}
+            aria-label="Contact Us"
+          >
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
