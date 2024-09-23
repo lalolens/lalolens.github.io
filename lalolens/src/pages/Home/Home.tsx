f204009c62b245055b9dc7becc47651d6060f19c
lalolens <elaloglens@gmail.com>
2024-09-23 00:13:09 +0000

    Deployment pipelne setup

--- lalolens/src/pages/Home/Home.tsx
+++ lalolens/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 // src/components/Home.tsx
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Home.css';
 import profilePic from '../../assets/profile.jpg'; // Ensure you have a profile picture in this path
 
 const Home: React.FC = () => {
+  const location = useLocation();
   return (
     <section className="home">
       <img src={profilePic} alt="Your Name" className="profile-pic" />
@@ -11,9 +13,14 @@ const Home: React.FC = () => {
       <p>
         I'm a dedicated Software Engineer with a passion for developing scalable and secure software solutions. With expertise in AI integration, cloud computing, and full-stack development, I transform complex challenges into innovative outcomes. Let's collaborate to bring ideas to life.
       </p>
-      <button className="cta-button" onClick={() => window.location.href = '/contact'}>
-        Get in Touch
-      </button>
+      
+      <Link
+            to="/contact"
+            className="cta-button"
+            aria-label="Contact Us"
+          >
+            Get in Touch
+          </Link>
     </section>
   );
 };
