b781a46d7f1967cca1f0716ddb0b4a670489b565
lalolens <elaloglens@gmail.com>
2024-09-23 20:30:14 +0000

    adding new design layout

--- lalolens.root/src/pages/Contact/Contact.tsx
+++ lalolens.root/src/pages/Contact/Contact.tsx
@@ -2,6 +2,12 @@
 import React from 'react';
 import './Contact.css';
 
+// Import your logo images (use correct paths for your logo files)
+import emailLogo from '../../assets/material-icons/mail.svg'; // Replace with actual path to your email logo
+import linkedinLogo from '../../assets/social-icons/linkedIn-logo.png'; // Replace with actual path to your LinkedIn logo
+import githubLogo from '../../assets/social-icons/github-logo.svg'; // Replace with actual path to your GitHub logo
+
+
 const Contact: React.FC = () => {
   return (
     <section className="contact">
@@ -9,12 +15,19 @@ const Contact: React.FC = () => {
       <p>
         I'm always open to discussing new opportunities, innovative ideas, or potential collaborations. Feel free to reach out via email or connect with me on LinkedIn and GitHub.
       </p>
-      <p>
-        <a href="mailto:elaloglens@gmail.com" target="_blank"><strong>Email</strong></a>
-        <a href="https://www.linkedin.com/in/eduardo-guzman-818a6a166/" target="_blank"><strong>LinkedIn</strong></a>
-        <a href="https://github.com/lalolens" target="_blank"><strong>GitHub</strong></a>
-      </p>
+      <div className="contact__logos">
+        <a href="mailto:elaloglens@gmail.com" target="_blank" rel="noopener noreferrer">
+          <img src={emailLogo} alt="Email" className="contact__logo mail_logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/eduardo-guzman-818a6a166/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedinLogo} alt="LinkedIn" className="contact__logo" />
+        </a>
+        <a href="https://github.com/lalolens" target="_blank" rel="noopener noreferrer">
+          <img src={githubLogo} alt="GitHub" className="contact__logo" />
+        </a>
+      </div>
     </section>
+
   );
 };
 
--- lalolens.root/src/pages/Home/Home.tsx
+++ lalolens.root/src/pages/Home/Home.tsx
@@ -4,22 +4,41 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 import profilePic from '../../assets/profile.jpg'; // Ensure you have a profile picture in this path
 
+
+const Hero: React.FC = () => {
+  return (
+    <>
+    <div className="hero">
+      <img src={profilePic} alt="Your Name" className="profile-pic hero__img" />
+      <h1 className="hero__heading">Driving Innovation Through Technology</h1>
+      <p className="hero__body">
+        I'm a dedicated Software Engineer with a passion for developing scalable and secure software solutions. With expertise in AI integration, cloud computing, and full-stack development, I transform complex challenges into innovative outcomes. Let's collaborate to bring ideas to life.
+      </p>
+    </div>
+    </>
+  );
+}
+
+const GetInTouch: React.FC = () => {
+  return (
+    <>
+    <Link
+      to="/contact"
+      className="cta-button"
+      aria-label="Contact Us"
+    >
+      Get in Touch
+    </Link>
+    </>
+  );
+}
+
 const Home: React.FC = () => {
   return (
     <section className="home">
-      <img src={profilePic} alt="Your Name" className="profile-pic" />
-      <h1>Driving Innovation Through Technology</h1>
-      <p>
-        I'm a dedicated Software Engineer with a passion for developing scalable and secure software solutions. With expertise in AI integration, cloud computing, and full-stack development, I transform complex challenges into innovative outcomes. Let's collaborate to bring ideas to life.
-      </p>
-      
-      <Link
-            to="/contact"
-            className="cta-button"
-            aria-label="Contact Us"
-          >
-            Get in Touch
-          </Link>
+      <Hero />
+      <GetInTouch />
+
     </section>
   );
 };
