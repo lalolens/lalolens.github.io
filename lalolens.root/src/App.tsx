e75a6a5eed74700a3d8f05d18ff3b55091f8d664
lalolens <elaloglens@gmail.com>
2024-09-24 08:54:29 +0000

    testing automation done

--- lalolens.root/src/App.tsx
+++ lalolens.root/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
