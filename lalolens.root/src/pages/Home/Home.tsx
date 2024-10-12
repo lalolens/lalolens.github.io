426d9bb4a3a7d4dbef1818dc9aa004e1d84ddec3
lalolens <elaloglens@gmail.com>
2024-10-12 02:59:16 +0000

    attempting to get github pages working

--- lalolens.root/src/pages/Home/Home.tsx
+++ lalolens.root/src/pages/Home/Home.tsx
@@ -11,10 +11,6 @@ import profilePic from '../../assets/profile.jpg'; // Ensure you have a profile 
 import './Home.css'; // Import the CSS file for styling
 import '../MainContent.css';
 
-import  Playground  from '@lalolens/playground/Playground';
-
-
-
 
 const Hero: React.FC = () => {
   return (
@@ -23,7 +19,6 @@ const Hero: React.FC = () => {
       <img src={profilePic} alt="Your Name" className="profile-pic hero__img" />
       <h1 className="hero__heading">Designing the Foundation for Innovation</h1>
       <p className="hero__body">
-      <Playground />
       Hi, I'm Eduardo, and this is my personal platform where I bring to life the projects, innovations, and achievements that define my journey in technology. Lalolens is a reflection of my drive to innovate solutions in the world of technology, offering a unique lens into my approach to problem-solving and creativity within the IT landscape. Join me as we push the boundaries, transform ideas into reality, and make a lasting impact.
       </p>
     </div>
