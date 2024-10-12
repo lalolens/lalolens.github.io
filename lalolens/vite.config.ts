9776e37322992cb9deccae6f82df8e3202a64a5b
lalolens <elaloglens@gmail.com>
2024-10-12 08:27:43 +0000

    .

--- lalolens/vite.config.ts
+++ lalolens/vite.config.ts
@@ -12,7 +12,25 @@
     },
     build: {
       outDir: 'dist',
-      sourcemap: true
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'dotcom/main.tsx'),
+          playground: path.resolve(__dirname, 'packages/playground/src/Playground.tsx'), // Update this to the main file of your playground package
+        },
+        output: {
+          dir: path.resolve(__dirname, 'dist'),
+          format: 'es', // Define the output format as ES modules
+          entryFileNames: '[name].js', // Specify entry file naming convention
+          chunkFileNames: '[name]-[hash].js', // Specify chunk file naming convention
+          assetFileNames: '[name]-[hash][extname]' // Specify asset file naming convention
+        },
+        external: [
+          // Specify dependencies to be treated as external (not bundled in the final output)
+          'react', 
+          'react-dom'
+        ]
+      }
     },
     server: {
       // Optional: If your project structure requires serving additional directories for development
